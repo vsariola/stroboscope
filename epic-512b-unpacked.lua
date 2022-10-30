2df44939b72f92757a6f34a8ea3b86ccb166780c
5684185+vsariola@users.noreply.github.com <5684185+vsariola@users.noreply.github.com>
2022-10-31 01:51:35 +0200

    Optimize size and fine tune.

--- epic-512b-unpacked.lua
+++ epic-512b-unpacked.lua
@@ -36,18 +36,19 @@ d = {
 t = 0
 
 function TIC()
+  --{
   for k = 3, 0, -1 do
     p = t // 896 -- orderlist pos
     poke(65896, 32) -- set chn 2 wave
-    e = t << d[k + 104] -- envelope pos
+    x = t << d[k + 104] -- envelope pos
     -- n is note (semitones), 0=no note
     n = d[
         16 * d[9 * k + p + 106] + 1 -- patstart
-            + e // 14 % 16] -- can sometimes be removed
+            + x // 14 % 16] -- can sometimes be removed
     -- save envelopes for syncs
     -- d[0] = chn 0, d[-1] = chn 1...
     -- % ensures if n=0|pat=0 then env=0
-    d[-k] = -e % 14 % (16 * n * d[9 * k + p + 106] + 1)
+    d[-k] = -x % 14 % (16 * n * d[9 * k + p + 106] + 1)
     u = d[4 * d[p + 95] + 1 + t // 224 % 4]
 
     n = ((n - 1 - u // 2 * 2) * 7 // 6 + u + 1) * 12 // 7
@@ -56,37 +57,36 @@ function TIC()
       8 -- global pitch:
       + 12 * k -- octave
       + n -- note
-      - k // 3 * e % 14 * 7 -- pitch drop
-      ~ 0, -- convert to int
+      - k // 3 * x % 14 * 7-- pitch drop
+      , -- convert to int
       2,
       k,
       d[-k]-- stored envelope
     )
   end
   cls(3)
-  for j = 0, 47 do
-    poke(16320 + j, 255 / (1 + 2 ^ (5 - s(j % 3 + p) - j / 5 - d[-3] * .2)) ^ 2)
+  for k = 0, 47 do
+    poke(16320 + k, 255 / (1 + 2 ^ (5 - s(k % 3 + p) - k / 5 - d[-3] / 5)) ^ 2)
   end
-
+  --}
   d[p % 3 + 142](
-    s(p % 8 * (s(t / 99) + t / 199)) * 52 + 120,
-    s(p % 8 * (s(t / 79) + t / 179)) * 52 + 68, 50, 0)
+    s(p % 8 * (s(t / 70) + t / 190)) * 52 + 120,
+    s(p % 8 * (s(t / 80) + t / 170)) * 52 + 68, 50, 0)
 
   for a = 13, 1, -1 do
-    u = a * a * (d[-3] + 14) / 400 / 14 + 1
+    u = a * a * (d[-3] + 14) / 6000 + 1
     for k = .5, 10 do
       y = (p + 1) % 3 // 2 * (1 - k / 5)
       r = (1 - y * y) ^ .5
-      e = p // 3 * 3.9
-      x = r * s(e * k + t / 18 + 8)
+      x = r * s(p // 3 * 4 * k + t / 18 + 8)
 
       d[p % 3 + 142](
-        x * 52 * u + s(p % 8 * (s(t / 99) + t / 199)) * 52 + 120,
-        y * 52 * u + s(p % 8 * (s(t / 79) + t / 179)) * 52 + 68,
-        a * math.atan(math.max(u * (x ^ 2 + y ^ 2) ^ .5 - 1, r * s(e * k + t / 18)) * 10) * d[-2] / 4 - 1,
+        x * 52 * u + s(p % 8 * (s(t / 70) + t / 190)) * 52 + 120,
+        y * 52 * u + s(p % 8 * (s(t / 80) + t / 170)) * 52 + 68,
+        a * math.atan(math.max(u * (x ^ 2 + y ^ 2) ^ .5 - 1, r * s(p // 3 * 4 * k + t / 18)) * 10) * d[-2] / 4 - 1,
         -a)
     end
-    d[144 - n // 2 % 2 * 2]((.5 - n % 2) * (d[0] - 6) * u * 52 + 120, 0, u * 10, -a)
+    d[144 - (n & 2)]((.5 - n % 2) * (d[0] - 6) * u * 52 + 120, 0, u * 10, -a)
   end
   t = t + 1, t < 8063 or exit()
 end
