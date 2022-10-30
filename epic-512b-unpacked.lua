f0467372cb3e4ef7b24cf94ad43c499522fe7bb4
5684185+vsariola@users.noreply.github.com <5684185+vsariola@users.noreply.github.com>
2022-10-31 01:20:03 +0200

    Finetuning and optimize size.

--- epic-512b-unpacked.lua
+++ epic-512b-unpacked.lua
@@ -13,22 +13,24 @@ d = {
   1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0,
   1, 7, 7, 1, 7, 7, 1, 7, 1, 7, 7, 1, 7, 7, 1, 7,
   1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0,
-  1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1,
+  1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, -- 1, 1,
 
-  -- start 97, length 4: channel speeds
+  -- 95 keylist
+  1, 1, 0, 0, 3, 3, 3, 2, 0,
+
+  -- start 104, length 4: channel speeds
   -2, 1, -- 0, 0, from following
 
-  -- 99 orderlist
+  -- 106 orderlist
   0, 0, 0, 5, 5, 1, 1, 1, 5,
   2, 2, 3, 3, 3, 3, 3, 3, 0,
   0, 1, 2, 2, 1, 1, 1, 1, 2,
   2, 2, 4, 5, 5, 5, 5, 5, 4,
-  -- 135 keylist
-  1, 1, 0, 0, 3, 3, 3, 2, 0,
-  -- 144
-  function(x, y, r, c) rect(0, x - r, 240, r * 2, c) end,
+
+  -- 142
+  function(x, r, r, c) rect(0, x - r, 240, r * 2, c) end,
   circ,
-  function(x, y, r, c) rect(x - r, 0, r * 2, 240, c) end,
+  function(x, r, r, c) rect(x - r, 0, r * 2, 240, c) end,
 }
 
 t = 0
@@ -37,16 +39,16 @@ function TIC()
   for k = 3, 0, -1 do
     p = t // 896 -- orderlist pos
     poke(65896, 32) -- set chn 2 wave
-    e = t << d[k + 97] -- envelope pos
+    e = t << d[k + 104] -- envelope pos
     -- n is note (semitones), 0=no note
     n = d[
-        16 * d[9 * k + p + 99] + 1 -- patstart
+        16 * d[9 * k + p + 106] + 1 -- patstart
             + e // 14 % 16] -- can sometimes be removed
     -- save envelopes for syncs
     -- d[0] = chn 0, d[-1] = chn 1...
     -- % ensures if n=0|pat=0 then env=0
-    d[-k] = -e % 14 % (16 * n * d[9 * k + p + 99] + 1)
-    u = d[4 * d[9 * 4 + p + 99] + 1 + t // 224 % 4]
+    d[-k] = -e % 14 % (16 * n * d[9 * k + p + 106] + 1)
+    u = d[4 * d[p + 95] + 1 + t // 224 % 4]
 
     n = ((n - 1 - u // 2 * 2) * 7 // 6 + u + 1) * 12 // 7
     sfx(
@@ -66,9 +68,9 @@ function TIC()
     poke(16320 + j, 255 / (1 + 2 ^ (5 - s(j % 3 + p) - j / 5 - d[-3] * .2)) ^ 2)
   end
 
-  d[p % 3 + 144](
-    s(p % 8 * (t / 199 + s(t / 99))) * 52 + 120,
-    s(p % 8 * (t / 179 + s(t / 79))) * 52 + 68, 50, 0)
+  d[p % 3 + 142](
+    s(p % 8 * (s(t / 99) + t / 199)) * 52 + 120,
+    s(p % 8 * (s(t / 79) + t / 179)) * 52 + 68, 50, 0)
 
   for a = 13, 1, -1 do
     u = a * a * (d[-3] + 14) / 400 / 14 + 1
@@ -76,16 +78,15 @@ function TIC()
       y = (p + 1) % 3 // 2 * (1 - k / 5)
       r = (1 - y * y) ^ .5
       e = p // 3 * 3.9
-      x = r * s(e * k + 8 + t / 29)
-      h = math.atan(math.max(u * (x ^ 2 + y ^ 2) ^ .5 - 1, r * s(e * k + t / 29)) * 10) * d[-2] / 4 - 1
+      x = r * s(e * k + t / 18 + 8)
 
-      d[p % 3 + 144](
-        52 * x * u + s(p % 8 * (t / 199 + s(t / 99))) * 52 + 120,
-        52 * y * u + s(p % 8 * (t / 179 + s(t / 79))) * 52 + 68,
-        (a + u) * h,
+      d[p % 3 + 142](
+        x * 52 * u + s(p % 8 * (s(t / 99) + t / 199)) * 52 + 120,
+        y * 52 * u + s(p % 8 * (s(t / 79) + t / 179)) * 52 + 68,
+        a * math.atan(math.max(u * (x ^ 2 + y ^ 2) ^ .5 - 1, r * s(e * k + t / 18)) * 10) * d[-2] / 4 - 1,
         -a)
     end
-    d[146 - n // 2 % 2 * 2]((.5 - n % 2) * (d[0] - 6) * u * 50 + 120, 0, u * 10, -a)
+    d[144 - n // 2 % 2 * 2]((.5 - n % 2) * (d[0] - 6) * u * 52 + 120, 0, u * 10, -a)
   end
   t = t + 1, t < 8063 or exit()
 end
